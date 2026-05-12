ad0f757ff08ed9a35e795c6970a9a20b68d2a994
sange <sange@192.168.1.200>
2026-05-12 13:01:15 +0400

    Committed

---
+++ src/test/java/Demo/Demo1Test.java
@@ -0,0 +1,15 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class Demo1Test {
+	
+	@Test(groups = "smoke")
+	
+	public void mangoTest()
+	{
+		System.out.println("mango");
+	}
+	
+
+}
---
+++ src/test/java/Demo/DemoTest.java
@@ -0,0 +1,16 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+@Test
+	
+public void appleTest()
+{
+	System.out.println("apple");
+}
+	
+	
+	
+}
